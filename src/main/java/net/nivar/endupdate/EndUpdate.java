672f9685d56a04213e554650f13f4fef0450706b
epiccerr <anivbalaji@gmail.com>
2025-02-16 22:29:30 -0500

    Initial commit version 1

---
+++ src/main/java/net/nivar/endupdate/EndUpdate.java
@@ -0,0 +1,16 @@
+package net.nivar.endupdate;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndUpdate implements ModInitializer {
+	public static final String MOD_ID = "end-update";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
